f3ec0783ae830afc62a55472dc93825507dcf035
yijunjun <junjunyi@gmail.com>
2019-02-25 20:01:12 +0800

    初始化项目

---
+++ go.mod
@@ -0,0 +1,3 @@
+module mod
+
+require github.com/yijunjun/modtest v1.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/yijunjun/modtest"
+)
+
+func main() {
+    fmt.Println(modtest.Hi("yijunjun"))
+}
